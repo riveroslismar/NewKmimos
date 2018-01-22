cfd2216f6f8cb90bcd5b6bd35e3ad2856729603c
Angel Veloz <vlzangel91@gmail.com>
2018-01-22 13:18:24 +0100

    Limpiando

--- bb2/master/inicio.php
+++
@@ -1,6 +0,0 @@
-<?php
-$sub=0; include('_header.php'); ?>
-
-<br><br><center><img src="img/Logo.png" height="300"></center><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-
-<?php include('_bottom.php'); ?>
--- bb2/master/salir.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$_SESSION['i'];
-$_SESSION['n'];
-unset($_SESSION['i']);
-unset($_SESSION['n']);
-session_destroy();
-header('Location: index.php');
-?>
